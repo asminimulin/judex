9ad1e13bf2498bbf451d448e7ecbf3fef1eab53f
exsandebest <dan56213@mail.ru>
2019-06-03 14:26:34 +0300

    Updated first_name, last_name on pages profile.php, user.php

--- judex.tech/profile.php
+++ judex.tech/profile.php
@@ -1,10 +1,10 @@
 <?php
  include "standart.php";
- $query = "select name from users where id = $userId";
+ $query = "select first_name, last_name from users where id = $userId";
  $result = mysqli_query($link, $query);
- $row = mysqli_fetch_row($result);
+ $userName  = mysqli_fetch_assoc($result);
  mysqli_free_result($result);
- $userName = $row[0];
+
 ?>
 <html>
 <head>
@@ -16,7 +16,7 @@
 <?php include "views/navbar.php";?>
 <center>
     <h1>Профиль</h1>
-    <h2><?php echo $userName;?></h2>
+    <h2><?php echo $userName["first_name"]." ".$userName["last_name"] ;?></h2>
     <h3>Ваш рейтинг: <?php
         $query = "select rating from users where id=$userId";
         $result = mysqli_query($link, $query);
--- judex.tech/user.php
+++ judex.tech/user.php
@@ -6,15 +6,15 @@ if (isset($_GET['id'])){
     if ($viewUserId == $userId){
     header("Location: /profile.php");
     }
-    $query = "select login, name, rating from users where id = $viewUserId";
+    $query = "select login, first_name, last_name, rating from users where id = $viewUserId";
     $result = mysqli_query($link, $query);
     $viewUserObj = mysqli_fetch_assoc($result);
     mysqli_free_result($result);
     if (!$viewUserObj){
-        header("Location: /404.php");
+        header("Location: /404.php#kek");
     }
 } else {
-    header("Location: /404.php");
+    header("Location: /404.php#lol");
 }
 
 
@@ -32,7 +32,7 @@ if (isset($_GET['id'])){
 <?php include "views/navbar.php";?>
 <center>
     <h1>Страница пользователя</h1>
-    <h2><?php echo $viewUserObj['login']." (".$viewUserObj['name'].")"; ?></h2>
+    <h2><?php echo $viewUserObj['login']." (".$viewUserObj['first_name']." ".$viewUserObj["last_name"].")"; ?></h2>
     <h3>Рейтинг: <?php echo $viewUserObj['rating'];?></h3>
     <input type="button" class="btn green small" value="Покекать" onclick="alert('Кек, кек, кек');">
 </center>
