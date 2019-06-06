e274e7c614e35facd3e6f8b9015b4012387af368
Alexey Minimulin <a.s.minimulin@gmail.com>
2019-06-07 00:00:40 +0300

    Created global.php - file where php gets all global constanst

---
+++ judex.tech/include/global.php
@@ -0,0 +1,6 @@
+<?php
+    $JUDEX_HOME = getenv("JUDEX_HOME");
+    $CONF = parse_ini_file("$JUDEX_HOME/conf.d/judex.conf", true);
+    $SUBMISSIONS = $CONF["testing"]["submissions_dir"];
+    $PROBLEMS = $CONF["archive"]["problems_dir"];
+?>
