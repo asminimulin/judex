e5b3f54318377411f2f45f611e133993432d7cc2
asminimulin <a.s.minimulin@gmail.com>
2019-08-07 15:38:12 +0300

    Fixed default password for admin

--- res/init.sql
+++ res/init.sql
@@ -242,7 +242,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin','admin@judex.tech','Admin','Admin',0,_binary '\0\0\0\0');
+INSERT INTO `users` VALUES (1,'admin',MD5('admin'),'admin@judex.tech','Admin','Admin',0,_binary '\0\0\0\0');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
