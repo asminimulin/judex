74d8d1857bc7b05f370f28c7cc68d1a58247b224
asminimulin <a.s.minimulin@gmail.com>
2019-08-12 11:40:10 +0300

    Fix more interpreter paths

--- installation/install.sh
+++ installation/install.sh
@@ -8,14 +8,14 @@ if [ "$EUID" != "0" ]; then
     exit 1
 fi
 
-/usr/bin/sh -c ./init-linux-user.sh
-/usr/bin/sh -c ./install-dependencies.sh
-/usr/bin/sh -c ./init-filesystem.sh
-/usr/bin/sh -c ./init-config.sh
-/usr/bin/sh -c ./init-database.sh
-/usr/bin/sh -c install-site.sh "judex.tech" "/opt/judex/www" "src/judex.tech"
-/usr/bin/sh -c ./init-languages.sh
-/usr/bin/sh -c ./init-testing-source.sh
+/bin/sh -c ./init-linux-user.sh
+/bin/sh -c ./install-dependencies.sh
+/bin/sh -c ./init-filesystem.sh
+/bin/sh -c ./init-config.sh
+/bin/sh -c ./init-database.sh
+/bin/sh -c install-site.sh "judex.tech" "/opt/judex/www" "src/judex.tech"
+/bin/sh -c ./init-languages.sh
+/bin/sh -c ./init-testing-source.sh
 
 tar -xf "res/problems.tar" -C "/var/lib/judex/Problems"
 
